bc96f522d361a7fc69955ccb4311df304bbcd705
Ashish Doneriya <ashishdoneriya@gmail.com>
2023-08-13 01:09:53 +0530

    Fixed image orientation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::error::Error;
 use std::fs;
+use std::io::BufWriter;
+use std::num::NonZeroU32;
 use std::os::unix::fs::MetadataExt;
 
 use actix_files::NamedFile;
 use actix_web::{App, get, HttpResponse, HttpServer, Responder, web};
-use image::GenericImageView;
-use image::imageops::FilterType;
+use fast_image_resize as fr;
+use image::{ColorType, ImageEncoder};
+use image::codecs::png::PngEncoder;
+use image::io::Reader as ImageReader;
 use rust_embed::RustEmbed;
 use serde::{Deserialize, Serialize};
 
@@ -39,7 +43,7 @@ async fn main() -> std::io::Result<()> {
 				web::scope("apis").service(list).service(thumbnail))
 			.service(index)
 			.service(dist)
-	})
+	}).keep_alive(None)
 		.bind("0.0.0.0:8080")?
 		.run()
 		.await
@@ -148,15 +152,11 @@ struct ThumbnailQuery {
 #[get("thumbnail")]
 async fn thumbnail(query: web::Query<ThumbnailQuery>, app_state: web::Data<AppState>) -> actix_web::Result<NamedFile> {
 	let final_path = app_state.generate_absolute_path1(&query.source_dir_path, &query.file_name);
-	println!("{}", final_path);
 	fs::metadata(&final_path)?;
 	let file = match create_thumbnail(&app_state, &final_path, &query.thumbnail_size) {
-		Ok(Some(target_file)) => {
+		Ok(target_file) => {
 			target_file
-		}
-		Ok(None) => {
-			final_path
-		}
+		},
 		Err(error) => {
 			eprintln!("Problem while creating thumbnail of image {}, ERROR - {:?}", &final_path, error);
 			final_path
@@ -166,48 +166,131 @@ async fn thumbnail(query: web::Query<ThumbnailQuery>, app_state: web::Data<AppSt
 	Ok(named_file)
 }
 
-fn create_thumbnail(app_state: &web::Data<AppState>, file_path: &String, thumbnail_size: &String) -> Result<Option<String>, Box<dyn Error>> {
-	let height: u32;
+
+fn create_thumbnail(app_state: &web::Data<AppState>, src_img_path: &String, thumbnail_size: &String)
+		-> Result<String, Box<dyn Error>> {
+	let new_height: u32;
 	if "small".eq(thumbnail_size) {
-		height = 320;
+		new_height = 320;
 	} else {
-		height = 720;
+		new_height = 720;
 	}
-	let relative_path = &file_path[app_state.base_dir.len() + 1..file_path.len()];
+	let relative_path = &src_img_path[app_state.base_dir.len() + 1..src_img_path.len()];
 	if relative_path.starts_with(".thumbnails-") {
-		return Ok(Some(file_path.to_string()));
+		return Ok(src_img_path.to_string());
 	}
-	let mut target_path = String::new();
-	target_path.push_str(&app_state.base_dir);
-	target_path.push(std::path::MAIN_SEPARATOR);
-	target_path.push_str(&".thumbnails-height-");
-	target_path.push_str(&height.to_string());
-	target_path.push(std::path::MAIN_SEPARATOR);
-	target_path.push_str(relative_path);
-	if fs::metadata(&target_path).is_ok() {
-		return Ok(Some(target_path));
+	let mut target_img_path = String::new();
+	target_img_path.push_str(&app_state.base_dir);
+	target_img_path.push(std::path::MAIN_SEPARATOR);
+	target_img_path.push_str(&".thumbnails-height-");
+	target_img_path.push_str(&new_height.to_string());
+	target_img_path.push(std::path::MAIN_SEPARATOR);
+	target_img_path.push_str(relative_path);
+	if fs::metadata(&target_img_path).is_ok() {
+		return Ok(target_img_path);
 	}
-	if let Some(parent_dir) = std::path::Path::new(&target_path).parent() {
+	if let Some(parent_dir) = std::path::Path::new(&target_img_path).parent() {
 		if !parent_dir.exists() {
 			fs::create_dir_all(parent_dir)?;
 		}
 	}
-	let img = image::open(&target_path)?;
-	let (img_width, img_height) = img.dimensions();
-	if img_height <= height {
-		let target = std::path::Path::new(&target_path);
-		let file = std::path::Path::new(&file_path);
-		symlink::symlink_file(target, file)?;
-		return Ok(Some(target_path));
+
+	match resize(&src_img_path, &target_img_path, new_height) {
+		Ok(_) => Ok(target_img_path),
+		Err(error) => {
+			println!("Couldn't generate thumbnail of {:?}, error - {:?}", src_img_path, error);
+			let target_file = std::path::Path::new(&target_img_path);
+			let src_file = std::path::Path::new(&src_img_path);
+			symlink::symlink_file(src_file, target_file)?;
+			Ok(target_img_path)
+		}
 	}
-	let width = (img_width * height) / img_height;
-	if let Err(error) = img.resize(width, height, FilterType::CatmullRom).save(&target_path) {
-		eprintln!("Problem while creating thumbnail of image {}, ERROR - {:?}", file_path, error);
-		let target = std::path::Path::new(&target_path);
-		let file = std::path::Path::new(&file_path);
-		symlink::symlink_file(target, file)?;
+}
+
+fn resize(src_img_path: &String, target_img_path: &String, new_height: u32) -> Result<(), Box<dyn Error>> {
+	let img = ImageReader::open(src_img_path)?.decode()?;
+	let rotation = get_image_rotation(src_img_path);
+	let img = match rotation { 0 => img,
+		90 => img.rotate90(),
+		180 => img.rotate180(),
+		270 => img.rotate270(),
+		_ => img
+	};
+	let old_height = img.height();
+	if new_height > old_height {
+		let target_file = std::path::Path::new(&target_img_path);
+		let src_file = std::path::Path::new(&src_img_path);
+		symlink::symlink_file(src_file, target_file)?;
+		return Ok(());
 	}
-	return Ok(Some(target_path));
+	let old_width = img.width();
+	let img_width = NonZeroU32::new(old_width).ok_or("Couldn't create NonZeroU32 for old_width")?;
+
+	let img_height = NonZeroU32::new(old_height).ok_or("Couldn't create NonZeroU32 for old_height")?;
+	let mut src_image = fr::Image::from_vec_u8(
+		img_width,
+		img_height,
+		img.to_rgba8().into_raw(),
+		fr::PixelType::U8x4,
+	)?;
+
+	// Multiple RGB channels of source image by alpha channel
+	// (not required for the Nearest algorithm)
+	let alpha_mul_div = fr::MulDiv::default();
+	alpha_mul_div.multiply_alpha_inplace(&mut src_image.view_mut())?;
+
+	let new_width = (old_width * new_height) / old_height;
+
+	// Create container for data of destination image
+	let dst_width = NonZeroU32::new(new_width).ok_or("Couldn't create NonZeroU32 for new_width")?;
+	let dst_height = NonZeroU32::new(new_height).ok_or("Couldn't create NonZeroU32 for new_height")?;
+	let mut dst_image = fr::Image::new(dst_width, dst_height, src_image.pixel_type());
+
+	// Get mutable view of destination image data
+	let mut dst_view = dst_image.view_mut();
+
+	// Create Resizer instance and resize source image
+	// into buffer of destination image
+	let mut resizer = fr::Resizer::new(fr::ResizeAlg::Nearest);
+	resizer.resize(&src_image.view(), &mut dst_view)?;
+
+	// Divide RGB channels of destination image by alpha
+	alpha_mul_div.divide_alpha_inplace(&mut dst_view)?;
+
+	// Write destination image
+	let mut result_buf: BufWriter<Vec<u8>> = BufWriter::new(Vec::new());
+	PngEncoder::new(&mut result_buf)
+		.write_image(
+			dst_image.buffer(),
+			dst_width.get(),
+			dst_height.get(),
+			ColorType::Rgba8,
+		)?;
+
+	// Save the result to the destination path
+	let result_content = result_buf.into_inner()?;
+	fs::write(target_img_path, &result_content)?;
+	Ok(())
+}
+
+fn get_image_rotation(file_path: &String) -> u32 {
+	if let Ok(exif) = rexif::parse_file(file_path) {
+		for entry in &exif.entries {
+			if entry.tag.to_string().eq("Orientation") {
+				let rotation = entry.value_more_readable.to_string();
+				if rotation.eq("Straight") {
+					return 0;
+				} else if rotation.eq("Rotated to left") {
+					return 90;
+				} else if rotation.eq("Rotated to right") {
+					return 270;
+				} else {
+					return 180;
+				}
+			}
+		}
+	}
+	return 0;
 }
 
 fn is_image(file_name: &String) -> bool {
